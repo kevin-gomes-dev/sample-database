f31a628a50964bb299eae6890d8a223afd9bca3b
plowww <kevin_g@my.uri.edu>
2022-11-29 18:08:29 -0500

    Updating app.js, rearranging file struct

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.use(express.static("src"));
+app.use('/', express.static('public'));
+
+// For invalid endpoints
+app.get('*',(req,res) => {
+  res.send('Error 404: Invalid endpoint (check URL)');
+});
+
 app.listen(port, () => {
-  console.log("Listening on port 3000");
+  console.log(`Listening on http://localhost:${port}`);
 });

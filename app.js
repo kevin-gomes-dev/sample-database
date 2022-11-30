d6fba2467c29e03d22d625d22e5b844fd9c80016
plowww <kevin_g@my.uri.edu>
2022-11-29 19:21:23 -0500

    Adding about, updating for 2 pages

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+/**
+ * The main place to setup serving files, what requests do what, and determining routing
+ */
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.use('/', express.static('public'));
+app.use(express.static("public"));
+
+app.use("/about", express.static("public/about.html"));
 
 // For invalid endpoints
-app.get('*',(req,res) => {
-  res.send('Error 404: Invalid endpoint (check URL)');
+app.get("*", (req, res) => {
+  res.send("Error 404: Invalid endpoint (check URL)");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+// Start server on given port
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
 });

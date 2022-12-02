bd20c1a4cce121e27ab0930a152ac0f8587e0bfb
plowww <kevin_g@my.uri.edu>
2022-12-02 14:54:01 -0500

    Adding generic update for PUT statements

--- database/db.js
+++ database/db.js
@@ -4,21 +4,21 @@
  * Most of this code was gotten and modified from:
  * https://www.terlici.com/2015/08/13/mysql-node-express.html
  */
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 
 // The 2 databases. Test for testing, prod for the actual data
 const PRODUCTION_DB = process.env.database || 'sample_database',
-  TEST_DB = 'test_' + process.env.database || 'test_sample_database'
+  TEST_DB = 'test_' + process.env.database || 'test_sample_database';
 
 // The two kinds of modes we can be in
-exports.MODE_TEST = 'mode_test'
-exports.MODE_PRODUCTION = 'mode_production'
+exports.MODE_TEST = 'mode_test';
+exports.MODE_PRODUCTION = 'mode_production';
 
 // The state of our DB
 const state = {
   pool: null,
   mode: null,
-}
+};
 
 /**
  * Makes the state's pool for the database connection. This is so we don't have to create a new
@@ -33,18 +33,18 @@ exports.connect = function (mode = MODE_TEST, done) {
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || '',
     database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB,
-  })
-  state.mode = mode
-  done(state)
-}
+  });
+  state.mode = mode;
+  done(state);
+};
 
 /**
  * Simply gets our pool
  * @returns {mysql.Pool} The state's pool (the connection)
  */
 exports.get = function () {
-  return state.pool
-}
+  return state.pool;
+};
 
 /**
  * Adds data from the JSON data param. Mainly for testing. Structure is as follows:
@@ -60,15 +60,15 @@ exports.get = function () {
  * @param {Function} done The function to call when we are all done
  */
 exports.fixtures = function (data = {}, done) {
-  const pool = state.pool
-  if (!pool) return done(new Error('Missing database connection.'))
-  const names = Object.keys(data.tables)
+  const pool = state.pool;
+  if (!pool) return done(new Error('Missing database connection.'));
+  const names = Object.keys(data.tables);
   names.forEach(function (name, cb) {
     data.tables[name].forEach(function (row, cb) {
       const keys = Object.keys(row),
         values = keys.map(function (key) {
-          return "'" + row[key] + "'"
-        })
+          return "'" + row[key] + "'";
+        });
 
       pool.query(
         'INSERT INTO ' +
@@ -79,10 +79,10 @@ exports.fixtures = function (data = {}, done) {
           values.join(',') +
           ')',
         cb
-      )
-    }, cb)
-  }, done)
-}
+      );
+    }, cb);
+  }, done);
+};
 
 /**
  * Deletes all data from all tables given
@@ -90,14 +90,14 @@ exports.fixtures = function (data = {}, done) {
  * @param {Function} done The callback when we are finished
  */
 exports.delete = function (tables = [], done) {
-  const pool = state.pool
-  if (!pool) return done(new Error('No database connection in pool'))
+  const pool = state.pool;
+  if (!pool) return done(new Error('No database connection in pool'));
 
   // For every table, delete everything, and then call the callback
   tables.forEach((table, callBack) => {
-    pool.query(`DELETE FROM ${table}`, callBack)
-  }, done)
-}
+    pool.query(`DELETE FROM ${table}`, callBack);
+  }, done);
+};
 
 /**
  * Adds an item defined by the body into the database on given table.
@@ -105,17 +105,40 @@ exports.delete = function (tables = [], done) {
  * It's also expected that the body matches the table structure exactly
  * @param {String} table The name of the table we insert into
  * @param {JSON} body The JSON we wish to add
+ * @param {Function} done The callback when we are done
  */
 exports.add = function (table, body, done) {
-  const pool = state.pool
-  const cols = Object.keys(body)
+  const pool = state.pool;
+  const cols = Object.keys(body);
   // For each column, we get the value
-  const values = cols.map((col) => "'" + body[col] + "'")
+  const values = cols.map((col) => "'" + body[col] + "'");
   pool.query(
     `INSERT INTO ${table} (${cols.join(',')}) VALUES (${values.join(',')})`,
     (err, result) => {
-      if (err) throw err
-      done(result)
+      if (err) throw err;
+      done(result);
     }
-  )
-}
+  );
+};
+
+/**
+ * This updates a table in the database with given condition, table, and data.
+ * Condition is expected to be an SQL query (ex: WHERE ID = 4)
+ * @param {String} table The name of the table we are updating
+ * @param {JSON} body The JSON containing student info we want to update
+ * @param {String} condition The SQL condition to be placed after WHERE
+ * @param {Function} done The callback when we are done
+ */
+exports.update = function (table, body, condition, done) {
+  const pool = state.pool;
+  const cols = Object.keys(body);
+  // Make the SET col = value, ... pairs here, separated by comma except the last item
+  const setPairs = cols.map((col) => `${col} = "${body[col]}"`).join(',');
+  pool.query(
+    `UPDATE ${table} SET ${setPairs} WHERE ${condition}`,
+    (err, result) => {
+      if (err) throw err;
+      done(result);
+    }
+  );
+};
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,36 +1,41 @@
 /**
  * The student router. To be used at endpoint /student
  */
-const express = require('express')
-const router = express.Router()
-const db = require('../database/db.js')
+const express = require('express');
+const router = express.Router();
+const db = require('../database/db.js');
 
 // Get all students
 router.get('/', (req, res) => {
-  const pool = db.get()
+  const pool = db.get();
   pool.query(`SELECT * FROM students`, (err, result) => {
-    if (err) throw err
-    res.send(result)
-  })
-})
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
 // Add a student
 router.post('/', (req, res) => {
-  db.add('students', req.body, (result) => {
-    res.send(result)
-  })
-})
+  db.add('students', req.body, (result) => res.send(result));
+});
 
 // Delete a student by id
 router.delete('/:id', (req, res) => {
-  const pool = db.get()
+  const pool = db.get();
   pool.query(
     `DELETE FROM students WHERE ID = ${req.params.id}`,
     (err, result) => {
-      if (err) throw err
-      res.send(result)
+      if (err) throw err;
+      res.send(result);
     }
-  )
-})
+  );
+});
 
-module.exports = router
+// Update a student by id
+router.put('/:id', (req, res) => {
+  const data = req.body;
+  const condition = `ID = ${req.params.id}`;
+  db.update('students', data, condition, (result) => res.send(result));
+});
+
+module.exports = router;

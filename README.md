9d8e0178c192825c5f4018db729fd83f68473a83
plowww <kevin_g@my.uri.edu>
2022-12-02 13:08:09 -0500

    Adding Postman collection, updating pages

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Sample Database
-Just a small repo to learn how to set up backend + frontend only using JS and frameworks.
+Just a small repo to learn how to set up backend + frontend only using JS and frameworks. I've avoided things like sequelize and mysql-js
+and other modules so I can make life harder for me (my chosen torture, if you will). That way I can be sure I know the inner workings of this stuff.
+This helps so when I start using abstraction to make things quicker, more consistent and easier, I will have a solid ground knowledge to understand what's going on.
+
 The host is defaulted to localhost. All tests for API will be available via Postman Collections which can
 be imported into Postman and ran in sequence or individually (ensure server is running).
 To run the backend, 3 small steps:
@@ -27,5 +30,7 @@ DATABASE=sample_database
 2. Run the createDb.js file using "npm run init"
 3. Run the main app using "npm run start"
 
-If you wish to stop the server, use CTRL + C (on windows) or the Mac/Linux equivalent
+While running the server, navigating to "http://localhost:YOUR_PORT" will go to the home page. Very much WIP, but gives some info about the project.
+
+If you wish to stop the server, use CTRL + C (on windows) or the Mac/Linux equivalent.
 If you wish to delete the database, run SQL query: DROP DATABASE "yourDatabaseName";
--- public/src/utils.js
+++ public/src/utils.js
@@ -7,47 +7,88 @@
  */
 export function setupNav() {
   // Our content area
-  const divMain = document.getElementById("content");
+  const divMain = document.getElementById('content');
 
   // Append a nav to the content
-  const navMain = document.createElement("nav");
-  navMain.id = "navMain";
+  const navMain = document.createElement('nav');
+  navMain.id = 'navMain';
   divMain.appendChild(navMain);
 
   // Create the ul for the links
-  const navLinks = document.createElement("ul");
-  navLinks.id = "navLinks";
+  const navLinks = document.createElement('ul');
+  navLinks.id = 'navLinks';
   navMain.appendChild(navLinks);
 
   // Append home link
-  appendLink(navLinks, "/", () => "Home");
+  appendLink(navLinks, '/', () => 'Home');
 
   // Append About Me link
-  appendLink(navLinks, "/about", () => "About");
+  appendLink(navLinks, '/about', () => 'About');
 }
 
 /**
  * Creates a p element and appends to main content area with given text
  * @param {String} text The text to display
  */
-export function setupIntroText(text) {
-  const divMain = document.getElementById("content");
-  const pElem = document.createElement("p");
-  pElem.innerHTML = text;
+export function addParagraph(text) {
+  const divMain = document.getElementById('content');
+  const pElem = document.createElement('p');
+  pElem.innerText = text;
   divMain.appendChild(pElem);
 }
 
 /**
- * Appends an a element to the given list. The reference and what to show as clickable are params
+ * Appends an <a> element to the given list. The reference and what to show as clickable are params
  * @param {HTMLElement} list The list we want to append to
  * @param {HTMLElement} ref Where this link will go to
  * @param {Function} display The function to determine what to display
  */
 function appendLink(list, ref, display) {
-  const liElem = document.createElement("li");
-  const linkElem = document.createElement("a");
+  const liElem = document.createElement('li');
+  const linkElem = document.createElement('a');
   linkElem.href = ref;
   linkElem.innerText = display();
   liElem.appendChild(linkElem);
   list.appendChild(liElem);
+}
+
+/**
+ * Adds a table to the content div with given data. Data is in 2D Array form where each outer list is the row,
+ * and its value is a list containing all data for the row
+ * @param {String} tableId The optional table's id in the HTML, if any at all
+ * @param {Array} data The data we wish to enter
+ */
+export function addTable(data, tableId) {
+  /* Example data:
+    [
+      ['Requests','Endpoints','Params'],
+      ['GET','students','']
+    ]
+   */
+  const divMain = document.getElementById('content'),
+    tableElem = document.createElement('table');
+  tableElem.style.border = '1px solid';
+
+  // If we gave an id, assign it to table
+  if (tableId) {
+    tableElem.id = tableId;
+  }
+
+  // For every row, get values and create <tr>
+  // For every list of values, create and append <td> to the <tr>
+  // Once done with values, ppend the row to the table and iterate next
+  // Finally, append table to main content area
+  for (const row of data) {
+    const tableRow = document.createElement('tr');
+    for (const value of row) {
+      const tableData = document.createElement('td');
+      tableData.style.border = '1px dotted';
+      tableData.style.paddingRight = '40px';
+      tableData.innerText = value;
+      tableRow.appendChild(tableData);
+    }
+
+    tableElem.appendChild(tableRow);
+  }
+  divMain.appendChild(tableElem);
 }

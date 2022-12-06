6af05780e6477c6b578f0a839b4c153d9af2a139
plowww <kevin_g@my.uri.edu>
2022-12-06 14:15:06 -0500

    Updating queries, added delete 1 course

--- routes/courseManagementRoute.js
+++ routes/courseManagementRoute.js
@@ -8,9 +8,6 @@ const Student = require('../models/studentModel.js');
 const StudentCoursesLink = require('../models/studentCoursesLinkModel.js');
 const Course = require('../models/courseModel.js');
 
-//TODO: Use param for id
-//TODO: Add missing requests
-
 // Add course to student (takes student id as param, course id in request body)
 router.post('/:id', (req, res) => {
   const studentId = req.params.id,
@@ -22,8 +19,8 @@ router.post('/:id', (req, res) => {
   // Looks scarier than it is
   db.getPool().query(
     `INSERT INTO ${StudentCoursesLink.tableName} (coursePriId,studentPriId)
-     SELECT ${courseTable}.Id, ${studentTable}.Id FROM ${courseTable},${studentTable} WHERE
-  ${courseTable}.CourseId = ${courseId} AND ${studentTable}.Id = ${studentId}`,
+     SELECT ${courseTable}.Id, ${studentTable}.Id FROM ${courseTable} AS courses,${studentTable} AS students WHERE
+  courses.CourseId = ${courseId} AND students.Id = ${studentId}`,
     (err, result) => {
       if (err) throw err;
       if (result.affectedRows > 0)
@@ -33,11 +30,23 @@ router.post('/:id', (req, res) => {
   );
 });
 
-// Add list of courses to student (takes student id from param, list of courses)
-
-// Delete course from student (takes student id from param, course id)
-
-// Delete list of courses from student (takes student id from param, list of courses)
+// Delete course from student (takes student id from param, course id from request body)
+router.delete('/:id', (req, res) => {
+  const studentId = req.params.id,
+    courseId = '"' + req.body.courseId + '"',
+    linkTable = StudentCoursesLink.tableName,
+    courseTable = Course.tableName;
+  db.getPool().query(
+    `DELETE link FROM ${linkTable} as link INNER JOIN ${courseTable} as courses
+    ON link.coursePriId = courses.Id WHERE
+  link.studentPriId = ${studentId} AND courses.courseId = ${courseId}`,
+    (err, result) => {
+      if (err) throw err;
+      if (result.affectedRows === 0) res.status(404).send();
+      else res.status(204).send();
+    }
+  );
+});
 
 // Delete all courses for all students
 router.delete('/', (_, res) => {
@@ -61,8 +70,8 @@ router.get('/:id', (req, res) => {
     linkTable = StudentCoursesLink.tableName,
     coursesTable = Course.tableName;
   db.getPool().query(
-    `SELECT * FROM ${coursesTable}, ${linkTable} WHERE 
-  ${linkTable}.studentPriId = ${studentId} AND ${linkTable}.coursePriId = ${coursesTable}.Id`,
+    `SELECT * FROM ${coursesTable} as courses, ${linkTable} as link WHERE 
+  link.studentPriId = ${studentId} AND link.coursePriId = courses.Id`,
     (err, result) => {
       if (err) throw err;
       if (result.length === 0) res.status(204).send();
